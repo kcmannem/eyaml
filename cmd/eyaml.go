3cb31ae2fb5061d3eef84e384294c07cf5c11321
kcmannem <chaitanyamannem@gmail.com>
2020-03-07 16:49:42 -0500

    adding encrypt cmd, but doesn't work for LiteralNodes

--- cmd/eyaml.go
+++ cmd/eyaml.go
@@ -6,7 +6,11 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/Shopify/ejson/crypto"
 	"github.com/fatih/color"
+	"github.com/goccy/go-yaml/ast"
+	p "github.com/goccy/go-yaml/parser"
+	"github.com/goccy/go-yaml/printer"
 	"github.com/shopify/ejson"
 	"github.com/spf13/cobra"
 )
@@ -17,6 +21,163 @@ var rootCmd = &cobra.Command{
 }
 
 const keyStoreDir = ".eyaml/keys/"
+
+var unencryptedFile string
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+type actionFunc func([]byte) ([]byte, error)
+
+func isMetadataNode(node *ast.MappingValueNode) bool {
+	return isPublicKeyNode(node) || isEncryptKeyNode(node)
+}
+
+const (
+	publicKeyIndicator = "public_key"
+	encryptIndicator   = "encrypt"
+)
+
+func isPublicKeyNode(node *ast.MappingValueNode) bool {
+	if node.Key.String() == publicKeyIndicator {
+		return true
+	}
+	return false
+}
+func isEncryptKeyNode(node *ast.MappingValueNode) bool {
+	if node.Key.String() == encryptIndicator {
+		return true
+	}
+	return false
+}
+
+func walk(node ast.Node, do actionFunc) {
+	switch nodeType := node.(type) {
+	case *ast.MappingNode:
+		for _, subnode := range nodeType.Values {
+			if !isMetadataNode(subnode) {
+				walk(subnode, do)
+			}
+		}
+	case *ast.MappingValueNode:
+		if !isMetadataNode(nodeType) {
+			digValues(nodeType.Value, do)
+		}
+	case *ast.SequenceNode:
+		for _, subnode := range nodeType.Values {
+			digValues(subnode, do)
+		}
+	}
+	return
+}
+
+func digValues(node ast.Node, do actionFunc) {
+	switch nodeType := node.(type) {
+	case *ast.MappingValueNode:
+		digValues(nodeType.Value, do)
+	case *ast.SequenceNode:
+		for _, subnode := range nodeType.Values {
+			digValues(subnode, do)
+		}
+	case *ast.LiteralNode:
+		// LiteralNode.Value points to a StringNode
+		digValues(nodeType.Value, do)
+	case *ast.StringNode:
+		encryptedBytes, err := do([]byte(nodeType.Value))
+		if err != nil {
+			fmt.Println(err)
+		}
+		nodeType.Value = string(encryptedBytes)
+	}
+	return
+}
+
+func walkOnSurface(node ast.Node) (eyamlMetadata, error) {
+	metadata := eyamlMetadata{}
+
+	switch nodeType := node.(type) {
+	case *ast.MappingNode:
+		for _, subnode := range nodeType.Values {
+			if isPublicKeyNode(subnode) {
+				metadata.PublicKey = subnode.String()
+			}
+			// if isEncryptKeyNode(subnode) {
+			// 	metadata.EncryptFields = subnode.Value
+			// }
+		}
+	}
+
+	if metadata.PublicKey == "" {
+		return metadata, fmt.Errorf("Could not find public key")
+	}
+
+	return metadata, nil
+}
+
+type eyamlMetadata struct {
+	PublicKey     string   `yaml:"public_key,omitempty"`
+	EncryptFields []string `yaml:"_encrypt,omitempty"`
+}
+
+var encryptCmd = &cobra.Command{
+	Use:   "encrypt",
+	Short: "encrypt values in a yaml file",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		if !fileExists(args[0]) {
+			fmt.Println("file doesn't exist")
+			return
+		}
+
+		data, err := ioutil.ReadFile(args[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		astFile, err := p.ParseBytes(data, 1)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, nodeTree := range astFile.Docs {
+			metadata, err := walkOnSurface(nodeTree)
+
+			var kp crypto.Keypair
+			err = kp.Generate()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			var rawPubKey [32]byte
+			copy(rawPubKey[:], metadata.PublicKey)
+			encrypter := kp.Encrypter(rawPubKey)
+
+			walk(nodeTree.Body, encrypter.Encrypt)
+		}
+
+		yamlPrinter := printer.Printer{}
+		for _, nodeTree := range astFile.Docs {
+			rawEncryptedData := yamlPrinter.PrintNode(nodeTree)
+			fmt.Print(string(rawEncryptedData))
+		}
+	},
+}
+
+// var decryptCmd = &cobra.Command{
+// 	Use:   "decrypt",
+// 	Short: "Decrypt an eYaml file",
+// 	Run: func(cmd *cobra.Command, args []string) {
+
+// 	},
+// }
 
 var write bool
 var keygenCmd = &cobra.Command{
@@ -67,6 +228,7 @@ func init() {
 		"write to keystore dir",
 	)
 	rootCmd.AddCommand(keygenCmd)
+	rootCmd.AddCommand(encryptCmd)
 }
 
 func Execute() {
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/kcmannem/eyaml
 go 1.13
 
 require (
-	github.com/Shopify/ejson v1.2.1 // indirect
+	github.com/Shopify/ejson v1.2.1
 	github.com/dustin/gojson v0.0.0-20160307161227-2e71ec9dd5ad // indirect
 	github.com/fatih/color v1.9.0
+	github.com/goccy/go-yaml v1.3.2
 	github.com/shopify/ejson v1.2.1
 	github.com/smartystreets/goconvey v1.6.4 // indirect
 	github.com/spf13/cobra v0.0.6
+	gopkg.in/yaml.v2 v2.2.2
 )
